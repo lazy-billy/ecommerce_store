3843abad379c3b9bcf24c6226199adae2f6f8af9
lazy-billy <dizzy95@list.ru>
2023-02-20 19:22:17 +0500

    ...

--- store/views.py
+++ store/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 from .models import Category, Product
 
+
+def categories(requests):
+    return {
+        'categories': Category.objects.all(),
+    }
+
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
