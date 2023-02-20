6e931700b5911c688ff075b293f477a09b07e882
lazy-billy <dizzy95@list.ru>
2023-02-20 22:57:35 +0500

    ...

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
@@ -7,6 +8,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -31,6 +35,9 @@ class Product(models.Model):
         verbose_name_plural = 'Products'
         ordering = ('-created',)
 
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+    
     def __str__(self):
         return self.title
     
