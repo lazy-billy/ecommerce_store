31acec805f01712888bc86704847d3eb9546f404
lazy-billy <dizzy95@list.ru>
2023-02-22 00:20:50 +0500

    ...

--- store/views.py
+++ store/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 
-def all_products(request):
-    products = Product.objects.all()
+def products_all(request):
+    products = Product.products.all()
     return render(request, 'store/home.html', {'products': products})
 
 
