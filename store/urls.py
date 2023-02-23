713a17606ed45c2af33b0e4d6d4fbf29b147d73b
lazy-billy <dizzy95@list.ru>
2023-02-23 21:46:51 +0500

    preps for basket app

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.products_all, name='products_all'),
-    path('<slug:slug>/', views.product_detail, name='product_detail'),
+    path('search/<slug:slug>/', views.product_detail, name='product_detail'),
     path('books/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ def products_all(request):
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'store/products/detail.html', {'product': product})
+    return render(request, 'store/products/single.html', {'product': product})
 
 
 def category_list(request, category_slug):
