631638483f67ed6a82c932691bfecdd0272a837c
lazy-billy <dizzy95@list.ru>
2023-03-01 23:27:14 +0500

    implementing account activation funtionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import redirect, render
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from .forms import RegistrationForm
 
@@ -15,4 +19,16 @@ def account_register(request):
             user.set_password(registerForm.cleaned_data['password'])
             user.is_active = False
             user.save()
-            
+            # Setup email
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk))
+            })
+            user.email_user(subject=subject, message=message)
+
+    else:
+        registerForm = RegistrationForm()
+        return render(request, 'account/registration/register.html', {'form': registerForm})
